60bc08b61533506d5b8ac45d7b73355152d29ed9
Uriel Garcilazo <garcilazo.uriel@gmail.com>
2025-04-10 14:10:03 -0400

    gh-pages

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="sublimat",
-    packages=find_packages(include=['sublimat*']),
-    install_requires=[
-        "pandas",  
-    ],
-    package_dir={'': '.'}, 
-)
